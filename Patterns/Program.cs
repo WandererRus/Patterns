274db7ea9a28cf9ee12f1f01260224a41af054cd
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2022-11-01 18:51:51 +0300

    Добавьте файлы проекта.

---
+++ Patterns/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPirog pirog;
+            PirogCherry pirogCherry = new PirogCherry();
+            PirogStrawberry ps = new PirogStrawberry();
+            pirog = pirogCherry;
+            pirog.AddNachinka();
+            pirog = ps;
+            pirog.AddNachinka();
+        }
+    }
+    interface IPirog 
+    {
+        void AddNachinka();
+    }
+    class PirogCherry : IPirog
+    {
+        public PirogCherry() 
+        {
+        }
+        public void AddNachinka()
+        {
+            Console.WriteLine("В пирог добавлена вишня");
+        }
+    }
+    class PirogStrawberry : IPirog
+    {
+        public PirogStrawberry()
+        {
+        }
+        public void AddNachinka()
+        {
+            Console.WriteLine("В пирог добавлена клубника");
+        }
+    }
+}
